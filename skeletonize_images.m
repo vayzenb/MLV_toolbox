4b9e38f853565a06d6be5fcb979462ad8a2856c0
vayzenb <vayzenberg90@gmail.com>
2023-05-09 11:16:51 -0400

    testing on masked images

--- skeletonize_images.m
+++ skeletonize_images.m
@@ -1,42 +1,60 @@
 %% Take directory of images and convert them to skeletons
 function skeletonize_images(input_dir, target_dir)
 
-addpath('/user_data/vayzenbe/GitHub_Repos/MLV_toolbox'); %add MLV toolbox to path
-setup; %setup MLV toolbox directories
+    %Example how to execture from command line
+    %matlab -batch "skeletonize_images('/lab_data/behrmannlab/image_sets/masked-ecoset/val/0001_man','/lab_data/behrmannlab/image_sets/skeletonized-ecoset/val/0001_man')"
 
-if ~exist(target_dir, 'dir')
-   mkdir(target_dir)
+    error_log_name = '/lab_data/behrmannlab/image_sets/skeletonized-ecoset/val/error_log.txt'
+    addpath('/user_data/vayzenbe/GitHub_Repos/MLV_toolbox'); %add MLV toolbox to path
+    setup; %setup MLV toolbox directories
+
+    if ~exist(target_dir, 'dir')
+    mkdir(target_dir)
+    end
+
+    tic
+    im_files=[dir(strcat(input_dir,'/*.jpg'));dir(strcat(input_dir,'/*.png'));dir(strcat(input_dir,'/*.JPEG'))];
+    im_files
+    %testing without parfor
+    parfor file_num = 1:length(im_files)
+        try
+            im = imread([im_files(file_num).folder,'/',im_files(file_num).name]); %read image
+
+            im_resize = imresize(im,[150,150]); %resize image for efficiency
+
+            img = traceLineDrawingFromRGB(im_resize); % convert image to edge map
+
+            im_overlay = renderLinedrawing(img,[], [150,150], 2, [0,0,0]); % convert edgemap to line drawing
+
+        
+            MAT = computeMAT(im_overlay);  %calcualte skeleton from edgemap
+            
+            mat_blur = imgaussfilt(double(MAT.skeleton),1); % expand skel values by blurring a bit
+            
+            
+            %binarize
+            mat_blur = imbinarize(mat_blur);
+            
+            %resize to 224x224
+            mat_blur = imresize(mat_blur,[224,224]);
+            
+            
+            
+            imwrite(mat_blur, [target_dir, '/',im_files(file_num).name])
+
+        catch
+            disp(['Error with file ',im_files(file_num).name])
+            %add file name to error log
+            fileID = fopen(error_log_name,'a');
+            fprintf(fileID,'%s\n',im_files(file_num).name);
+            fclose(fileID);
+        end
+        
+        
+        
+    end
+    toc
+
+    
+
 end
-
-tic
-im_files=[dir(strcat(input_dir,'/*.jpg'));dir(strcat(input_dir,'/*.png'));dir(strcat(input_dir,'/*.JPEG'))];
-im_files
-parfor file_num = 1:length(im_files)
-    file_num
-    im = imread([im_files(file_num).folder,'/',im_files(file_num).name]); %read image
-
-    im_resize = imresize(im,[226,226]); %resize image for efficiency
-
-    img = traceLineDrawingFromRGB(im_resize); % convert image to edge map
-
-    im_overlay = renderLinedrawing(img,[], [500,500], 2, [0,0,0]); % convert edgemap to line drawing
-
-    
-    MAT = computeMAT(im_overlay);  %calcualte skeleton from edgemap
-    
-    mat_blur = imgaussfilt(double(MAT.skeleton),2); % expand skel values by blurring a bit
-    
-    
-    %binarize
-    mat_blur = imbinarize(mat_blur);
-    
-    %blur again so values aren't so extreme
-    mat_blur = imgaussfilt(double(mat_blur),2);
-    
-    
-    imwrite(mat_blur, [target_dir, '/',im_files(file_num).name])
-    
-    
-    
-end
-toc
